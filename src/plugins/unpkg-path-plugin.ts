7c32fa0955c934088d91f4e0b91bbe94c493605f
Hemanshu Mahajan <hemanshu.mahajan@gmail.com>
2023-01-29 21:06:59 +0530

    Added Cache Layer

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+  name: "filecache",
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -40,12 +45,28 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        //Check if files are already in cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        //If yes, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
