b6112ed2eba5d520fb330fc8becd4a661a8f1f37
Hemanshu Mahajan <hemanshu.mahajan@gmail.com>
2023-02-08 17:38:02 +0530

    improve editor and error handling

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,23 +1,37 @@
+import "./preview.css";
 import React, { useEffect, useRef } from "react";
 
 interface PreviewProps {
   code: string;
+  err: string;
 }
 
 const html = `
   <html>
     <head>
+    <style>
+      html {background-color: white;}
+    </style>
     </head>
     <body>
       <div id="root"></div>
       <script>
+      const handleError = (err) => {
+        const root = document.querySelector('#root');
+        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+        console.error(err);
+      }
+
+      window.addEventListener('error', (event) => {
+        event.preventDefault();
+        handleError(event.error);
+      })
+
         window.addEventListener('message', (event) => {
          try{
            eval(event.data)
          } catch(err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-            console.error(err);
+          handleError(err);
          }
         }, false)
       </script>
@@ -25,21 +39,27 @@ const html = `
   </html>
   `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
 
   return (
-    <iframe
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+      {err && <div className="preview-error">{err}</div>}
+    </div>
   );
 };
 
